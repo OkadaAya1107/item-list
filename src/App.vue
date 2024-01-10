c3f0379390ac66fde517461b547007fa1a38d532
Aya Okada <ayaokada@AyanoMacBook-Air.local>
2024-01-11 06:30:16 +0900

    プロップス

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <h1>Item List</h1>
+<item-list :items="itemList"></item-list>
+</template>
+
+<!-- Option API -->
+<!-- <script>
+import ItemList from './components/ItemList.vue';
+export default {
+components: {
+  ItemList,
+ },
+ data() {
+  return {
+    itemList: [
+      {
+       id: 1,
+       name: 'Item 1',
+    },
+      {
+       id: 2,
+       name: 'Item 2',
+    },
+      {
+       id: 3,
+       name: 'Item 3',
+    },
+   ],
+  };
+ },
+};
+</script> -->
+
+<!-- Composition API NEW -->
+<script setup>
+import ItemList from './components/ItemList.vue';
+
+import { ref } from 'vue';
+
+const itemList = ref([
+{
+       id: 1,
+       name: 'Item 1',
+    },
+      {
+       id: 2,
+       name: 'Item 2',
+    },
+      {
+       id: 3,
+       name: 'Item 3',
+    },
+]);
+</script>
+
+<style>
+</style>
---
+++ src/components/ItemList.vue
@@ -0,0 +1,24 @@
+<template>
+<ul>
+  <li v-for="item in items" :key="item.id">{{ item.id }} - {{ item.name }}</li>
+</ul>
+</template>
+
+<!-- Option API -->
+<!-- <script> 
+export default {
+  //プロップスでリストデータを親から受け取る
+  props: ['items'],
+};
+
+</script> -->
+
+<!-- Composition API NEW -->
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps(['items']);
+</script>
+
+<style>
+</style>
